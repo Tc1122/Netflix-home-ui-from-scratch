dbcea982c361f53b9043d3096133c09e0daa6ada
ansif <ansiftccapcha@gmail.com>
2022-07-02 23:08:19 +0530

    Create a Navbar folder inside Component folder  for js and css files of Navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" className='Netflix-logo' alt="" />
+        <img src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" className='avatar' alt="" />
+    </div>
+  )
+}
