45586ae72d995600d14d5d7473ef2ad6f5e03186
ansif <ansiftccapcha@gmail.com>
2022-07-06 12:19:58 +0530

    import apikey,imageurl,axios and useEffect in rowpost.s file

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { Axios } from 'axios'
+import {api_Key,image_Url} from '../../constants/constants'
 
 function RowPost() {
   return (
